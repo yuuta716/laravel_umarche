0ca232da3f4681bdf544dc0f1e469c47be8fb6c6
Yuta Nagahara <yuuta716saka@icloud.com>
2024-06-14 04:51:55 +0900

    属性設定

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,3 +1,5 @@
 <x-tests.app>
+    <x-slot name="header">header1</x-slot>
 コンポーネントテスト１
+<x-tests.card title="タイトル" content="本文"/>
 </x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,4 @@
 <x-tests.app>
+    <x-slot name="header">header2</x-slot>
     コンポーネントテスト2
 </x-tests.app>
