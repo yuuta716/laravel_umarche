5591c6de8ffbcce9138ac38fd1336ac9b91567db
Yuta Nagahara <yuuta716saka@icloud.com>
2024-07-06 15:25:08 +0900

    stock

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\shop;
 use App\Models\SecondaryCategory;
 use App\Models\Image;
+use App\Models\Stock;
 
 class Product extends Model {
     use HasFactory;
@@ -21,6 +22,10 @@ class Product extends Model {
 
     public function imageFirst() {
         return $this->belongsTo( Image::class, 'image1', 'id' );
+    }
+
+    public function stock() {
+        return $this->hasMany( stock::class );
     }
 
 }
