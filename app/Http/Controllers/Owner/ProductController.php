11259fcec333ffec5e1805270b79c4bedeac39a5
Yuta Nagahara <yuuta716saka@icloud.com>
2024-07-06 09:36:59 +0900

    engerLouding

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -4,17 +4,42 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Image;
+use App\Models\Owner;
+use App\Models\Product;
+use App\Models\SecondaryCategory;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct()
+    {
+    //オーナーかどうかの確認
+    $this->middleware("auth:owners");
+    $this->middleware(function ($request, $next) {
+    $id = $request->route()->parameter("product"); //productのidを取得
+    if (!is_null($id)) { //productのidが存在するなら↓
+    $productOwnerId = Product::findOrFail($id)->shop->owner->id;//Productの中にownerがないのでshop-
+    // >owner->idにする。
+    $productId = (int)$productOwnerId; //⽂字列=>数値に
+    //認証⽤のid↓
+    if ($productId !== Auth::id()) { //同じではなかったら
+    abort(404); //404の画⾯表⽰
+    } }return $next($request);});}
+
     public function index()
     {
-        //
+        //Owner::findOrFail(Auth::$id())でログインしているオーナーの情報を取得している。
+        //->shop->product更にこのコードで取得した情報からリレーションで繋がっているshopからproductを取得
+        // して変数で置き換える。
+        // $products = Owner::findOrFail(Auth::id())->shop->product;
+        $ownerInfo = Owner::with("shop.product.imageFirst")
+        ->where("id",Auth::id())->get();
+        // dd($ownerInfo);
+        //return view("owner.products.index"に$products（変数)に置き換えたものを使いたいので
+        // compact("products")で使⽤可能に！
+        return view("owner.products.index",compact("ownerInfo"));
+        //このview("owner.products.index"を制作する
     }
 
     /**
---
+++ resources/views/owner/products/index.blade.php
@@ -0,0 +1,39 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <x-flash-message status="session('status')" />
+                    <div class="flex justify-end mb-4">
+                        {{-- 変更前 route('owner.images.create') --}}
+                        <button onclick="location.href='{{ route('owner.products.create') }}'"
+                            class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded
+    text-lg">新規登録</button>
+                    </div>
+                    <div class="flex flex-wrap">
+                        {{-- 変更前 @foreach ($images as $image) --}}
+                        @foreach ($ownerInfo as $owner)
+                            @foreach ($owner->shop->product as $product)
+                                <div class="w-1/4 p-2 md:p-2">
+                                    {{-- 変更前 route('owner.images.edit', ['image' => $image->id]) --}}
+                                    <a href ="{{ route('owner.products.edit', ['product' => $product->id]) }}">
+                                        <div class="border rounded-md p-2 md:p-4">
+                                            {{-- 変更前 <x-thumbnail :filename="$image->filename" type="products" /> --}}
+                                            <x-thumbnail :filename="$product->imageFirst->filename" type="products" />
+                                            {{-- 変更前 <div class="text-xl"> {{ $image->title }} </div> --}}
+                                            {{-- 最初はないのでコメントに <div class="text-xl"> {{ $product->name }} </div> --}}
+                                        </div>
+                                    </a>
+                                </div>
+                            @endforeach
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+</x-app-layout>
