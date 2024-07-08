451bd6c2b4fe47649211a1e149e3d384e8ec9b12
Yuta Nagahara <yuuta716saka@icloud.com>
2024-07-08 14:04:12 +0900

    route chenge

--- routes/owner.php
+++ routes/owner.php
@@ -24,9 +24,9 @@ use App\Http\Controllers\Owner\ImageController; //ImageControllerの読み込み
 |
 */
 
-Route::get('/', function () {
-    return view('owner.welcome');
-});
+// Route::get('/', function () {
+//     return view('owner.welcome');
+// });
 
 Route::prefix("shops")
 ->middleware("auth:owners")->group(function(){
@@ -55,10 +55,10 @@ Route::get('/dashboard', function () {
 })->middleware(['auth:owners'])->name('dashboard');
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
+    // Route::get('register', [RegisteredUserController::class, 'create'])
+    //     ->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    // Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
