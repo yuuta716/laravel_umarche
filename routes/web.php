7bf5f3f4ae896d5a16bb6b50283a6c5e5d8483b0
Yuta Nagahara <yuuta716saka@icloud.com>
2024-07-09 13:34:51 +0900

    rとcとv

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComponentTestController;
 use App\Http\Controllers\LifeCycleTestController;
+use App\Http\Controllers\User\ItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,10 +19,13 @@ use App\Http\Controllers\LifeCycleTestController;
 Route::get('/', function () {
     return view('user.welcome');
 });
+Route::middleware("auth:users")->group(function(){
+    Route::get("/",[ItemController::class,"index"])->name("items.index");
+    });
 
-Route::get('/dashboard', function () {
-    return view('user.dashboard');
-})->middleware(['auth:users'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('user.dashboard');
+// })->middleware(['auth:users'])->name('dashboard');
 
 Route::get('/component-test1', [ComponentTestController::class,
 'showComponent1']);
