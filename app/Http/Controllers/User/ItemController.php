34918496d7698995af2a6e115fc0ea8f73a93f88
Yuta Nagahara <yuuta716saka@icloud.com>
2024-07-13 04:46:21 +0900

    商品在庫

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use Illuminate\Support\Facades\DB;
+use App\Models\Stock;
 
 class ItemController extends Controller
 {
@@ -50,6 +51,11 @@ class ItemController extends Controller
     public function show($id)
     {
         $product = Product::findOrFail($id);
-        return view('user.show', compact('product'));
+        $quantity = Stock::where('product_id', $product->id)->sum('quantity'); //⼀つの商品の在庫情報を取るた
+        // めに
+        if ($quantity > 9) {
+            $quantity = 9;
+        } //9より⼤きかったら９
+        return view('user.show', compact('product','quantity'));
     }
 }
