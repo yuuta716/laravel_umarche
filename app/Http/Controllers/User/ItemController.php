71535bb4922f12859a8d102ddda888e9b245c2a3
Yuta Nagahara <yuuta716saka@icloud.com>
2024-07-21 11:23:34 +0900

    queue

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -10,6 +10,7 @@ use App\Models\Stock;
 use App\Models\PrimaryCategory;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\TestMail;
+use App\Jobs\SendThanksMail;
 
 class ItemController extends Controller
 {
@@ -33,8 +34,9 @@ class ItemController extends Controller
 
     public function index(Request $request)
     {
-        Mail::to('test@example.com') //受信者の指定
-             ->send(new TestMail()); //Mailableクラス
+        // Mail::to('test@example.com') //受信者の指定
+        //      ->send(new TestMail()); //Mailableクラス
+        SendThanksMail::dispatch();
         $categories = PrimaryCategory::with("secondary")->get();
         $products = Product::availableItems()
         ->searchKeyword($request->keyword)
